77dce3b76fbd4d6c759a024d8e42e8ffb090bae2
Mikołaj Zatorski <41756225+MikeyZat@users.noreply.github.com>
2021-05-16 14:57:09 +0200

    Add cli (#2)

--- cli.js
+++ cli.js
@@ -1,1 +1,27 @@
-const test = () => {};
+#!/usr/bin/env node
+
+const yargs = require('yargs');
+const main = require('./index');
+
+const builder = (command) =>
+  command
+    .option('configFile', {
+      describe: 'Path to the detest config file',
+      type: 'string',
+      default: 'detest.conf',
+      alias: 'c',
+    })
+    .options({});
+
+const handler = ({ configFile, ...rest }) => main(configFile, rest);
+
+yargs
+  .command('*', 'Elo', builder, handler)
+  .usage('Usage: $0 [configFile] [options...]')
+  .example('$0 -c localTests.conf', 'Run local tests')
+  .help()
+  .alias('h', 'help')
+  .alias('v', 'version')
+  .epilogue('For more information, visit https://github.com/MikeyZat/detest')
+  .wrap(90)
+  .parse();
--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-const test = () => {};
+const main = (file_name, options) => {
+  console.log(file_name);
+  console.log(options);
+};
+
+module.exports = main;
